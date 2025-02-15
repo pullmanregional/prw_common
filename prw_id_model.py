fbd59f9ac071d2d8af8db201e75118c913e47fcd
Jonathan Lee <jonjlee@gmail.com>
2025-02-14 21:19:00 -0800

    Remove FK between PrwIdDetails and PrwId - this will be enforced in code and makes dropping tables more complicated on ingest

--- prw_id_model.py
+++ prw_id_model.py
@@ -23,7 +23,7 @@ class PrwId(PrwIdModel, table=True):
 class PrwIdDetails(PrwIdModel, table=True):
     __tablename__ = "prw_id_details"
     id: int | None = Field(default=None, primary_key=True)
-    prw_id: str = Field(foreign_key="prw_ids.prw_id", max_length=24)
+    prw_id: str = Field(unique=True, index=True, max_length=24)
     mrn: str = Field(unique=True, index=True, max_length=24)
     name: str | None = None
     dob: date | None = None
@@ -34,4 +34,7 @@ class PrwIdDetails(PrwIdModel, table=True):
     phone: str | None = None
     email: str | None = None
 
-    prw_id_ref: PrwId = Relationship(back_populates="details")
+    prw_id_ref: PrwId = Relationship(
+        back_populates="details",
+        sa_relationship_kwargs={"foreign_keys": "[PrwIdDetails.prw_id]"}
+    )
